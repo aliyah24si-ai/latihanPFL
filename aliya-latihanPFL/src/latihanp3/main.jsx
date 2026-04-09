3f9350de917c5f6ad8731566bf3c47b92f33cd4d
aliyah <aliyah24si@mahasiswa.pcr.ac.id>
2026-04-09 21:12:52 +0700

    mindahin folder latihan

---
+++ aliya-latihanPFL/src/latihanp3/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./tailwind.css";
+import BiayaKirimForm from "./BiayaKirimForm";
+
+createRoot(document.getElementById("root")).render(<BiayaKirimForm />);
--- aliya-latihanPFL/vite.config.js
+++ aliya-latihanPFL/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        latihanp2: resolve(__dirname, 'latihanp2.html'),
+        latihanp3: resolve(__dirname, 'latihanp3.html'),
+        latihanp4: resolve(__dirname, 'latihanp4.html'),
+      },
+    },
+  },
 })
